04bf765105a5b4540090e572b635979c414f27b8
jerineaso <jerin.easo.20@gmail.com>
2021-10-02 20:38:28 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let nextBtn = document.querySelector(".nextbtn");
+let backbtn = document.querySelector(".backbtn");
+
+// Next Button 
+// nextBtn.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     location.replace("/finalSubmit.html");
+// })
+
+// backbtn.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     location.replace("/index.html")
+// })
